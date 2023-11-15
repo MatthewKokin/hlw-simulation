2ac401d6818a5e671d81318fb81e8bf4429a2808
MatthewKokin <135328101+MatthewKokin@users.noreply.github.com>
2023-11-15 20:00:07 +0000

    feature: basic graph implemented

--- JS/main.js
+++ JS/main.js
@@ -49,5 +49,45 @@ function increment() {
     dateEl.textContent = "November " + count;
     wasteVolume = setOperationalDays(days)
     wasteEl.textContent = "Total waste / tonnes: " + wasteVolume
+    updateChart(count, wasteVolume);
 }
 dateBtn.addEventListener('click', increment)
+
+
+
+let chart = null;
+let yearsData = [];
+let wasteData = [];
+
+function setupChart() {
+    const ctx = document.getElementById('wasteChart').getContext('2d');
+    chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: yearsData, // X-axis labels
+            datasets: [{
+                label: 'Total Waste (tonnes)',
+                data: wasteData, // Y-axis data
+                fill: false,
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+
+function updateChart(year, waste) {
+    yearsData.push("November " + year);
+    wasteData.push(waste);
+    chart.update();
+}
+
+// Initialize the chart
+setupChart();
