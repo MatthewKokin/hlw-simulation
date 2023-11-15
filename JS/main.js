7e6744afe387e91843a60cd2757c0f5e11855918
MatthewKokin <135328101+MatthewKokin@users.noreply.github.com>
2023-11-15 20:21:37 +0000

    chore: refactor main.js

--- JS/main.js
+++ JS/main.js
@@ -1,41 +1,4 @@
-import PowerPlant from './PowerPlant.js';
-import data from './reactors_data.js';
-
-function createPowerPlants() {
-    for (let plant of data) {
-        plant.name = new PowerPlant(plant.name, plant.capacity, plant.days_before_closing);
-    }
-}
-
-let wasteVolume = 0
-function setOperationalDays(days) {
-    for (let plant of data) {
-        wasteVolume += plant.name.operate(days);
-    }
-    return wasteVolume
-}
-
-// function seeStatus() {
-//     let totalWaste = 0;
-//     let totalElectricity = 0;
-//     let header = 'Power Plant           Capacity (GW)    Operational Days         Nuclear Waste (tons)      Expected closure (days)    Elec generated (GW)';
-//     console.log(header);
-//     console.log('-'.repeat(header.length));
-
-//     for (let plant of data) {
-//         let powerPlant = plant.name;
-//         console.log(`${powerPlant.name.padEnd(20)} ${powerPlant.capacity.toString().padEnd(15)} ${powerPlant.operationalDays.toString().padEnd(25)} ${powerPlant.nuclearWaste.toString().padEnd(25)} ${powerPlant.daysBeforeClosing.toString().padEnd(25)} ${powerPlant.generatedElectricity}`);
-//         totalWaste += powerPlant.nuclearWaste;
-//         totalElectricity += powerPlant.generatedElectricity;
-//     }
-
-//     console.log('-'.repeat(header.length));
-//     console.log("Total Nuclear Waste Generated: ", totalWaste, "metric tons");
-//     console.log("Total Electricity Generated: ", totalElectricity, "GW");
-//     console.log('-'.repeat(header.length));
-// }
-
-createPowerPlants();
+import {createPowerPlants, setOperationalDays} from './utils.js'
 
 const dateEl = document.getElementById("date-el")
 const dateBtn = document.getElementById("increment-btn")
@@ -43,15 +6,20 @@ const wasteEl = document.getElementById("waste-value")
 
 let count = 2023;
 let days = 0;
+let wasteVolume = 0
+
 function increment() {
     count += 1;
     days += 365;
     dateEl.textContent = "November " + count;
-    wasteVolume = setOperationalDays(days)
+    wasteVolume += setOperationalDays(days)
     wasteEl.textContent = "New waste generated / m^3: " + wasteVolume
     updateChart(count, wasteVolume);
 }
 dateBtn.addEventListener('click', increment)
+
+createPowerPlants();
+
 
 
 
---
+++ JS/utils.js
@@ -0,0 +1,37 @@
+import PowerPlant from './PowerPlant.js';
+import data from './reactors_data.js';
+
+export function createPowerPlants() {
+    for (let plant of data) {
+        plant.name = new PowerPlant(plant.name, plant.capacity, plant.days_before_closing);
+    }
+}
+
+export function setOperationalDays(days) {
+    let wasteVolume = 0
+    for (let plant of data) {
+        wasteVolume += plant.name.operate(days);
+    }
+    return wasteVolume
+}
+
+
+// function seeStatus() {
+//     let totalWaste = 0;
+//     let totalElectricity = 0;
+//     let header = 'Power Plant           Capacity (GW)    Operational Days         Nuclear Waste (tons)      Expected closure (days)    Elec generated (GW)';
+//     console.log(header);
+//     console.log('-'.repeat(header.length));
+
+//     for (let plant of data) {
+//         let powerPlant = plant.name;
+//         console.log(`${powerPlant.name.padEnd(20)} ${powerPlant.capacity.toString().padEnd(15)} ${powerPlant.operationalDays.toString().padEnd(25)} ${powerPlant.nuclearWaste.toString().padEnd(25)} ${powerPlant.daysBeforeClosing.toString().padEnd(25)} ${powerPlant.generatedElectricity}`);
+//         totalWaste += powerPlant.nuclearWaste;
+//         totalElectricity += powerPlant.generatedElectricity;
+//     }
+
+//     console.log('-'.repeat(header.length));
+//     console.log("Total Nuclear Waste Generated: ", totalWaste, "metric tons");
+//     console.log("Total Electricity Generated: ", totalElectricity, "GW");
+//     console.log('-'.repeat(header.length));
+// }
